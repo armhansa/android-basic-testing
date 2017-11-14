506f50037d18a108dc0954ae759eef5ade6f06bb
Arm Hansa <armhansa@gmail.com>
2017-11-15 01:18:49 +0700

    Add Method onRevertClick

--- app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
+++ app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
@@ -3,20 +3,48 @@ package cc.somkiat.basicunittesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.time.Year;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userNameInput;
+    EditText emailInput;
+    DatePicker dateOfBirthInput;
+
+    android.text.format.Time now;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setValue();
     }
+
+    private void setValue() {
+        userNameInput = findViewById(R.id.userNameInput);
+        emailInput = findViewById(R.id.emailInput);
+        dateOfBirthInput = findViewById(R.id.dateOfBirthInput);
+
+        now = new android.text.format.Time();
+    }
+
+
 
     public void onSaveClick(View view) {
         //TODO
     }
 
     public void onRevertClick(View view) {
-        //TODO
+        now.setToNow();
+        userNameInput.setText("");
+        emailInput.setText("");
+        dateOfBirthInput.updateDate(now.year, now.month, now.monthDay);
     }
 }
