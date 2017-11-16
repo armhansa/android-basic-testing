8aa94236247c4a3eb64044aac49b126efb75d70c
Arm Hansa <armhansa@gmail.com>
2017-11-17 01:08:23 +0700

    MainActivityTest4: case name and email not successful

--- app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/MainActivityTest.java
@@ -103,6 +103,22 @@ public class MainActivityTest {
 
     }
 
+    @Test
+    public void mainActivityTest4() {
+        setValue();
+        SystemClock.sleep(1000);
+        nameText.perform(scrollTo(), replaceText("Hansathon_lnw999"), closeSoftKeyboard());
+        SystemClock.sleep(1000);
+        emailText.perform(scrollTo(), replaceText("armhansa@"), closeSoftKeyboard());
+        SystemClock.sleep(1000);
+        emailText.perform(pressImeActionButton());
+        saveBtn.perform(scrollTo(), click());
+        SystemClock.sleep(5000);
+
+    }
+
+    
+
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
